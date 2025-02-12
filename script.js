29b1a04dede02a1176a8f115a310b51e2cffed24
SurveenShahi <159697634+SurveenShahi@users.noreply.github.com>
2025-02-12 23:23:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I love you for being meri Valentine BBG, meri jaaneman😘";
+  question.innerHTML = "YAYYYY thank u for being my valentine my darling, ill buy kinder joy for you choohi";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
